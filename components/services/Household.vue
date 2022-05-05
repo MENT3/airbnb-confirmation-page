2032fa6d1f22af831a04dc1c5b4e60e2995a696f
Clément <clement.ramoslage@gmail.com>
2022-05-05 18:07:00 +0200

    feat: add calendar

--- components/services/Household.vue
+++ components/services/Household.vue
@@ -3,11 +3,40 @@
     <h1>
       Ménage
     </h1>
+
+    <div>
+      <no-ssr>
+        <v-date-picker v-model="selectedDate">
+          <template v-slot="{ inputValue, inputEvents }">
+            <input
+              class="bg-white border px-2 py-1 rounded"
+              :value="inputValue"
+              v-on="inputEvents"
+            />
+          </template>
+        </v-date-picker>
+      </no-ssr>
+
+      <div>
+        <ul>
+          <li
+            v-for="i in 10"
+            :key="i"
+          >
+            Coiffeur {{ i }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </Modal>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      selectedDate: new Date()
+    }
+  }
 }
 </script>
