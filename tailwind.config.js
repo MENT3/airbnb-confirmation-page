703e524954f9d5150d70429d4cf4088e61e782dc
Clément <clement.ramoslage@gmail.com>
2022-05-05 17:47:22 +0200

    refactor: tailwind import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   theme: {
-    container: {
-      center: true,
-      padding: '13rem'
-    }
-  }
+    extend: {
+      container: {
+        center: true,
+        padding: '13rem'
+      },
+    },
+  },
+  plugins: [],
 }
